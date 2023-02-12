7d96897c0f8bee8ef1047310a5876c7d5ff71162
Piyush Mundada <30878967+piymundada@users.noreply.github.com>
2023-02-12 10:38:05 +0100

    Fix to get Instance from service discovery
    when we use rest template, we need annotate the bean with @LoadBalanced to get Instance from Load balancer.

--- movieapp-UriUsingServiceDiscovery-Eureka/src/main/java/com/example/movieapp/MovieappApplication.java
+++ movieapp-UriUsingServiceDiscovery-Eureka/src/main/java/com/example/movieapp/MovieappApplication.java
@@ -3,12 +3,12 @@ package com.example.movieapp;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -31,6 +31,7 @@ public class MovieappApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();
 	}
